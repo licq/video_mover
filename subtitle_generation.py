48ab88de5e12c70e0c71d9cfe915a5afc6e025a0
licq <lee.chaoqun@gmail.com>
2024-02-22 19:30:33 +0800

    change default model to medium

--- subtitle_generation.py
+++ subtitle_generation.py
@@ -1,6 +1,6 @@
 import whisper
 
-model = whisper.load_model('large')
+model = whisper.load_model('medium')
 
 audio = whisper.load_audio('data/gpt.oga')
 audio = whisper.pad_or_trim(audio)
@@ -13,4 +13,6 @@ print(f"Detected language: {max(probs, key=probs.get)}")
 options = whisper.DecodingOptions()
 result = whisper.decode(model, mel, options)
 
+with open('result.txt', 'w') as f:
+    f.write(result.text)
 print(result.text)
